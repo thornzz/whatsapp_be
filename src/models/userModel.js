0e709e20749f9d15fa4b6f2a232322ff9ef1eb61
Ibrahim Akgun <ibrahim@k2mbilisim.com>
2023-12-18 00:34:41 +0300

    first commit

---
+++ src/models/userModel.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcrypt";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Lütfen isminizi giriniz"],
+    },
+    email: {
+      type: String,
+      required: [true, "Lütfen email adresinizi giriniz"],
+      unqiue: [true, "Bu email adresi zaten kullanılmaktadır"],
+      lowercase: true,
+      validate: [validator.isEmail, "Lütfen geçerli bir email adresi giriniz"],
+    },
+    picture: {
+      type: String,
+      default:process.env.DEFAULT_PICTURE
+    },
+    status: {
+      type: String,
+      default:process.env.DEFAULT_STATUS
+    },
+    password: {
+      type: String,
+      required: [true, "Lütfen şifrenizi giriniz"],
+      minLength: [
+        6,
+        "Şifrenizin en az 6 karakter uzunluğunda olması gerekmektedir",
+      ],
+      maxLength: [
+        128,
+        "Şifreniz en fazla 128 karakter olabilir",
+      ],
+    },
+  },
+  {
+    collection: "users",
+    timestamps: true,
+  }
+);
+userSchema.pre("save", async function (next) {
+  try {
+    if (this.isNew) {
+      const salt = await bcrypt.genSalt(12);
+      const hashedPassword = await bcrypt.hash(this.password, salt);
+      this.password = hashedPassword;
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+const UserModel =
+  mongoose.models.UserModel || mongoose.model("UserModel", userSchema);
+
+export default UserModel;
---
+++ src/services/auth.service.js
@@ -0,0 +1,89 @@
+import createHttpError from "http-errors";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import { UserModel } from "../models/index.js";
+
+//env variables
+const { DEFAULT_PICTURE, DEFAULT_STATUS } = process.env;
+
+export const createUser = async (userData) => {
+  const { name, email, picture, status, password } = userData;
+
+  //check if fields are empty
+  if (!name || !email || !password) {
+    throw createHttpError.BadRequest("Lütfen tüm alanları doldurun");
+  }
+
+  //check name length
+  if (
+    !validator.isLength(name, {
+      min: 2,
+      max: 25,
+    })
+  ) {
+    throw createHttpError.BadRequest(
+      "Tam isminizin 2 ila 16 karakter arasında olması gerekiyor."
+    );
+  }
+
+  //Check status length
+  if (status && status.length > 64) {
+    throw createHttpError.BadRequest(
+      "Durum bilginiz en fazla 64 karakter olabilir"
+    );
+  }
+
+  //check if email address is valid
+  if (!validator.isEmail(email)) {
+    throw createHttpError.BadRequest(
+      "Geçerli bir email adresi girdiğinizden emin olun."
+    );
+  }
+
+  //check if user already exist
+  const checkDb = await UserModel.findOne({ email });
+  if (checkDb) {
+    throw createHttpError.Conflict(
+      "Lütfen başka bir email adresi deneyin, bu adres zaten mevcut."
+    );
+  }
+
+  //check password length
+  if (
+    !validator.isLength(password, {
+      min: 6,
+      max: 128,
+    })
+  ) {
+    throw createHttpError.BadRequest(
+      "Şifreniz 6 ila 128 karakter arasında olması gerekmektedir."
+    );
+  }
+
+  //hash password--->to be done in the user model
+
+  //adding user to databse
+  const user = await new UserModel({
+    name,
+    email,
+    picture: picture || DEFAULT_PICTURE,
+    status: status || DEFAULT_STATUS,
+    password,
+  }).save();
+
+  return user;
+};
+
+export const signUser = async (email, password) => {
+  const user = await UserModel.findOne({ email: email.toLowerCase() }).lean();
+
+  //check if user exist
+  if (!user) throw createHttpError.NotFound("Geçersiz giriş bilgisi.");
+
+  //compare passwords
+  let passwordMatches = await bcrypt.compare(password, user.password);
+
+  if (!passwordMatches) throw createHttpError.NotFound("Geçersiz giriş bilgisi.");
+
+  return user;
+};
